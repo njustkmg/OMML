d21779506b2162fbbc1ef31a1ff9c02c5e6820cc
br <37184443+baoran@users.noreply.github.com>
2022-01-06 09:59:19 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="PaddleMM")
     parser.add_argument('--gpu', type=int, default=1)
-    parser.add_argument('--config', type=str, default='configs/sgraf_sgr.yml',
+    parser.add_argument('--config', type=str, default='configs/cmml.yml',
                         help='Path to model configuration param file.')
-    parser.add_argument('--data_root', type=str, default='/home/br/PDMM/COCO_new',
+    parser.add_argument('--data_root', type=str, default='data/COCO',
                         help='Folder to dataset, include eg. dataset_coco.json, img_feat.npy, img_box.npy and label.npy.')
-    parser.add_argument('--image_root', type=str, default='/home/br/PDMM/COCO_new/images',
+    parser.add_argument('--image_root', type=str, default='data/COCO/images',
                         help='Folder to original image file.')
     parser.add_argument('--out_root', type=str, default='experiment/cmml',
                         help='Folder to save experiment data, include model and log.')
