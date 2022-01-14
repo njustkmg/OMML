386258fedda6f0df90ca4dba5b572c4d6b0f3041
br <37184443+baoran@users.noreply.github.com>
2022-01-14 16:32:44 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="PaddleMM")
     parser.add_argument('--gpu', type=int, default=1)
-    parser.add_argument('--config', type=str, default='configs/retrieval/scan_i2t_avg.yml',
+    parser.add_argument('--config', type=str, default='configs/retrieval/cmml.yml',
                         help='Path to model configuration param file.')
     parser.add_argument('--data_root', type=str, default='data/COCO',
                         help='Folder to dataset, include eg. dataset_coco.json, img_feat.npy, img_box.npy and label.npy.')
     parser.add_argument('--image_root', type=str, default='data/COCO/images',
                         help='Folder to original image file.')
-    parser.add_argument('--out_root', type=str, default='experiment/scan_i2t_avg',
+    parser.add_argument('--out_root', type=str, default='experiment/cmml',
                         help='Folder to save experiment data, include model and log.')
     config = parser.parse_args()
 
